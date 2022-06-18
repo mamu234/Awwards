663570d9391a7a12c8e12e899c64e52386f952e3
carolyne <pcmaunda@gmail.com>
2022-06-18 23:17:21 +0300

    created template for registration

--- appawwards/views.py
+++ appawwards/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
+from django.contrib import messages
+from .forms import UserRegisterForm
 # Create your views here.
 
 def home(request):
     posts =Post.objects.all()
     return render(request,'index.html',{'posts':posts})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) 
+        if form.is_valid():
+            form.save() # Save user to Database
+            username = form.cleaned_data.get('username') # Get the username that is submitted
+            messages.success(request, f'Account created for {username}!') # Show sucess message when account is created
+            return redirect('home') # Redirect user to Homepage
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
--- projectawwards/settings.py
+++ projectawwards/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appawwards',
+    'bootstrap4',
     'crispy_forms',
 ]
 
