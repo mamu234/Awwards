1f543a0919b3df00ab347deb33156c94349c8a00
carolyne <pcmaunda@gmail.com>
2022-06-23 00:27:24 +0300

    created test for models

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,24 +1,17 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path,include
+from django.conf.urls import url
 from . import views
-from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework import routers
+from django.conf.urls.static import static
 
-
-
-
-urlpatterns = [
-        path('',views.index, name="index"),
-        path('register/',views.register,name="register"),
-        path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-        path('profile/',views.profile,name='profile'),
-        path('logout',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-        path('upload/',views.Upload_Project,name="upload_project"),
-        
-    
-
-] 
-
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^profile/(?P<user_id>\d+)?$', views.profile, name='profile'),
+    url(r'^update/profile$', views.updateprofile, name='updateprofile'),
+    url(r'^vote/(?P<post_id>\d+)?$', views.vote, name='vote'),    
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/post/$', views.PostList.as_view()),
+    url(r'^api/profile/$', views.ProfileList.as_view()),
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,88 +1,83 @@
-from django.shortcuts import render,redirect
-from .forms import *
-from django.contrib import messages
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from . models import Post, Profile
+from django.contrib.auth.models import User
+from . forms import NewPostForm, UserForm, ProfileForm 
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-from .models import Project
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . models import Post, Profile
 
+import datetime as dt
 
-def register(request):
-    if request.method == 'POST':
-        form =UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request,f"You have succesfully created an account. Proceed to Login")
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    context = {
-        'form':form
-    }
-    return render(request,"users/register.html",context)
-                    
-
+@login_required(login_url='/accounts/login/')
 def index(request):
-    title = "home"
-    projects = Project.objects.all().order_by('-pk')
-    context = {
-        "title":title,
-        "projects":projects,
-    }
-    
-    return render(request,"index.html",context)   
-     
+    current_user = request.user.id
+    user = request.user
+    date = dt.date.today()
+    posts = Post.objects.all()
+    if Profile.objects.filter(user = request.user).count() == 0:
+        prof = Profile(user=request.user)
+        prof.save()
+    return render(request, 'index.html',{"date": date, "posts": posts})
 
-@login_required
-def Upload_Project(request):
+@login_required(login_url='/accounts/login/')
+def profile(request,user_id=None):
+    if user_id == None:
+        user_id=request.user.id
+    current_user = User.objects.get(id = user_id)
+    user = current_user
+    images = Post.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login')
+def updateprofile(request):
+	if request.method == 'POST':
+		form = ProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('profile')
+
+	else:
+			form = ProfileForm()
+	return render(request, 'updateprofile.html',{"form":form })
+
+@login_required(login_url='/accounts/login/')
+def vote(request,post_id):
+    try:
+        post = Post.objects.get(id = post_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"vote.html", {"post":post})
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
     current_user = request.user
-    if request.method == "POST":
-        form = ProjectUploadForm(request.POST,request.FILES)
-        if form.is_valid():
-            project = form.save(commit=False)
-            project.user = current_user
-            project.save()
-            return redirect('home')
-    else:
-        form = ProjectUploadForm()
-    context = {
-        "form":form
-    }
-
-    return render(request,"project/project_upload.html",context)  
-
-
-@login_required    
-def profile(request):
-    user=request.user
-    my_profile=Profile.objects.get(user=user)
-    return render(request,"users/profile.html",{'my_profile':my_profile,"user":user})
-       
-    
-@login_required
-def update_profile(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile) 
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile') # Redirect back to profile page
+        form = NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            return redirect('index')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form":form})
+
+def search_results(request):
+
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_by_user(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"profile": searched_profiles})
 
     else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
-    return render(request, 'users/profile.html', context)
-    
 
 
